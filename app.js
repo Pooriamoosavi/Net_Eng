b3e904d14994bbc52473628cd8aa832daa5b3336
pooriamoosavi <pouriaux@gmail.com>
2020-03-28 18:51:04 +0430

    Error Handling

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const memory=require('./memoryManager')
 const turf=require('@turf/turf')
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const GJV=require('geojson-validation')
 
+
+//----------------------------Server Initialization------------------------------//
 const app = express()
+const getErrMsg='Invalid Request : Please pass the lat and long in the URL. ex:/gis/testpoint/35.322456/50.23445';
+const putErrMsg={
+  bodyMissing:'Bad Request : Body is missing!',
+  wrongFormat:'Bad Request : Wrong format - No polygon has been added',
+  internal:'Internal Error'
+};
 
-// parse application/json
+
+// parse application/json to fix the put requests
 app.use(bodyParser.json())
 
+
+//Load the initial file
 const initialization=(req,res,next)=>{
   memory.init()
   next()
@@ -15,18 +27,44 @@ const initialization=(req,res,next)=>{
 
 app.use(initialization);
 
+
+//-------------------Request Handling-------------------//
+//GET Request Handling
 app.get('/gis/testpoint/:lat/:long', function (req, res) {
-  var point=turf.point([req.params.long,req.params.lat])
-  var result=memory.search(point)
-  res.send(result)
+    var point=turf.point([req.params.long,req.params.lat])
+    var result=memory.search(point)
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(result)) 
 })
 
+//Error Handling In GET Request
+app.get('/gis/testpoint/',function(req,res){
+  res.header("Content-Type",'application/json');
+  res.send(errGen(400,getErrMsg))
+})
+
+app.get('/gis/testpoint/:prop',function(req,res){
+  res.header("Content-Type",'application/json');
+  res.send(errGen(400,getErrMsg))
+})
+
+//PUT Request Handling
 app.put('/gis/addpolygon', function(req,res){
-  var err=memory.add(req.body)
-  if(err==='500')
-    res.send('500')
-  else
-    res.send(memory.print()+ "\n 200 OK")
+  res.header("Content-Type",'application/json');
+  if(req.body==undefined){
+    res.send(errGen(400,putErrMsg.bodyMissing))
+  }else if(!GJV.valid(req.body)){
+    res.send(errGen(400,putErrMsg.wrongFormat))
+  }else{
+    var err=memory.add(req.body)
+    if(err==='500')
+      res.send(errGen(500,putErrMsg.internal))
+    else
+      res.send({
+        "code" : 200,
+        "result" :memory.print()
+      })
+  }
 
 })
  
@@ -36,3 +74,14 @@ if (port == null || port == "") {
 }
 app.listen(port);
 console.log('App Is Ready...')
+
+
+//-------------------Tools---------------------//
+const errGen=(errcode,errmsg)=>{
+  return JSON.stringify({
+    code:errcode,
+    msg:errmsg
+  })
+}
+
+
